90f861a8bc4354a8ec72e7d1677b7cd94c28be74
Elverth Pulido <alexth117@hotmail.com>
2023-05-31 11:56:30 -0500

    ajuste para en actualizar pagoConceptos

--- routes/pagoconcepto.router.js
+++ routes/pagoconcepto.router.js
@@ -8,7 +8,15 @@ const service = new PagoConceptoService();
 router.post('/update-conceptos', async (req, res, next) => {
   try {
     const conceptosActualizar = req.body;
-    const result = await service.updatePagoConceptoValor(conceptosActualizar);
+    console.log(conceptosActualizar);
+    const conceptosArray = Array.isArray(conceptosActualizar)
+      ? conceptosActualizar
+      : [conceptosActualizar];
+    const result = await Promise.all(
+      conceptosArray.map(async (concepto) => {
+        return await service.updatePagoConceptoValor(concepto);
+      })
+    );
     res.status(201).json(result);
   } catch (error) {
     next(error);
