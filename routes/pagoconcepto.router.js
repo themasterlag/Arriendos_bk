d8bb4012a99b3dc50c50a33fd17ea2ff038160b1
Robert-a11ly <rabquintero@hotmail.com>
2023-05-31 12:04:42 -0500

    ajuste

--- routes/pagoconcepto.router.js
+++ routes/pagoconcepto.router.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const PagoConceptoService = require('./../services/pagoconcepto.service');
 const service = new PagoConceptoService();
 
-router.post('/update-conceptos', async (req, res, next) => {
+router.patch('/update-conceptos', async (req, res, next) => {
   try {
     const conceptosActualizar = req.body;
     console.log(conceptosActualizar);
     const conceptosArray = Array.isArray(conceptosActualizar)
       ? conceptosActualizar
-      : [conceptosActualizar];
+      : conceptosActualizar;
     const result = await Promise.all(
       conceptosArray.map(async (concepto) => {
         return await service.updatePagoConceptoValor(concepto);
