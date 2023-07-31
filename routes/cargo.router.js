b61eee313f046f86ce4c92fd5826a605356a3a3b
Elverth Pulido <alexth117@hotmail.com>
2023-07-31 11:51:06 -0500

    feature endpoint para los cargos

---
+++ routes/cargo.router.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const router = express.Router();
+
+const CargoService = require('./../services/cargos.service')
+const cargoService = new CargoService()
+
+router.get('/', async (req, res, next)=>{
+    try {
+        const cargos = await cargoService.find()
+        res.json(cargos).status(200)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/', async(req, res, next)=>{
+    try {
+        const cargo = req.body
+        const newCargo = await cargoService.create(cargo)
+        res.json(newCargo).status(200)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.patch('/update', async(req, res, next)=>{
+    try {
+        const oldCargo = req.body
+        const idCargo = oldCargo.id_cargo
+        const updated = await cargoService.update(idCargo, oldCargo)
+        res.json(updated).status(200)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:id', async (req, res, next)=>{
+    try {
+        const { id } = req.params
+        const cargo = await cargoService.findOneCargo(id)
+        res.status(cargo).status(200)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete('/:id', async(req, res, next)=>{
+    try {
+        const { id } = req.params
+        const deleted = await cargoService.delete(id)
+        res.json(deleted).status(200)
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports = router
---
+++ services/cargos.service.js
@@ -0,0 +1,31 @@
+const con = require('../libs/sequelize')
+
+class CargoService{
+    constructor(){
+
+    }
+
+    async create(data) {
+        const cargo = await con.models.cargo.create(data);
+        return cargo;
+      }
+    async find(){
+        const cargos = await con.models.cargo.findAll()
+        return cargos
+    }
+    async findOneCargo(id){
+        const cargo = await con.models.cargo.findByPk(id)
+        return cargo
+    }
+    async update(id, data){
+        const cargo = await this.findOneCargo(id)
+        const rta = await cargo.update(data)
+        return rta
+    }
+    async delete(id){
+        const cargo = await this.findOneCargo(id)
+        await cargo.destroy()
+        return 'eliminado'
+    }
+}
+module.exports = CargoService
