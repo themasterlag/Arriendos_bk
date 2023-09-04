38391febca315c2cb0baaf59b85d135a4d971467
crondrakj <101365082+crondrakj@users.noreply.github.com>
2023-09-04 15:49:29 -0500

    router y service
    hice router y services

--- services/entidadBancaria.js
+++ services/entidadBancaria.js
@@ -6,6 +6,11 @@ class EntidadBancariaService{
 
   }
 
+  async create(data) {
+    const entidad_bancaria = await con.models.entidad_bancaria.create(data);
+    return entidad_bancaria;
+  }
+   
   async find(){
     const data = await con.models.entidad_bancaria.findAll({
       order: [
@@ -18,10 +23,45 @@ class EntidadBancariaService{
 
   async findOne(id){
     const rta = await con.models.entidad_bancaria.findByPk(id);
-    if(!rta){
-      throw {message: 'no se encontro', codigo:404};
+    if(!rta){ 
+      throw new Error('Entidad bancaria no encontrada');
     }
     return rta;
   }
+
+  async findByNombre(nombre) {
+    const rta = await con.models.banco.findOne({
+      where: {
+        nombre_banco: nombre
+      }
+    });
+  
+    if (!rta) {
+      throw { message: 'No se encontró el banco', codigo: 404 };
+    }
+  
+    return rta;
+  }
+  
+
+  async update(id, data){
+    const entidad_bancaria = await this.findOne(id)
+    const rta = await entidad_bancaria.update(data)
+    return rta
+}
+
+async delete(id){
+  const entidad_bancaria = await this.findOne(id)
+  await entidad_bancaria.destroy()
+  return 'eliminado'
+}
+
+
+
+async modify(id, nuevoNombre) {
+  const entidad_bancaria = await this.findOne(id);
+  const rta = await entidad_bancaria.update({ entidad_bancaria: nuevoNombre }); // Actualizar el nombre
+  return rta;
+}
 }
 module.exports = EntidadBancariaService;
